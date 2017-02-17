670d79946b365878445fb91ce7b4a50c18095694
Simon Baev <simonbaev@gmail.com>
2017-02-17 12:32:08 -0500

    with data

---
+++ 2017a/data.js
@@ -0,0 +1,891 @@
+console.log("HELLO");
+window.mtData = {
+	date: "02/12/2016",
+	questions: {
+		ALGE: {
+			1: 4,
+			12: 5,
+			13: 4,
+			14: 1,
+			15: 3,
+			16: 4,
+			17: 2,
+			18: 1,
+			23: 3,
+			24: 4,
+			31: 2,
+			34: 4
+		},
+		ANGE: {
+			4: 4,
+			5: 1,
+			6: 1,
+			7: 2
+		},
+		GEOM: {
+			2: 1,
+			3: 4,
+			19: 5,
+			22: 4,
+			25: 1,
+			30: 5,
+			32: 4,
+			37: 4
+		},
+		TRIG: {
+			8: 2,
+			9: 3,
+			10: 4,
+			11: 3,
+			20: 1,
+			21: 2,
+			26: 5,
+			27: 1,
+			28: 3,
+			29: 3
+		},
+		MISC: {
+			33: 4,
+			35: 2,
+			36: 2,
+			38: 5,
+			39: 2,
+			40: 3,
+		}
+	},
+	teams: [
+		{
+			division: "GHSA A",
+			schools: [
+				{
+					name: "Turner County High School",
+					students: [
+						"DeAndre Roy Pierce",
+						"Kayla Jones",
+						"Alexus Raineu",
+						"Dominique Camille Farrie",
+						"Monique Jaliyah Farrie",
+						"Quentin Tennile",
+						"Dakyrae Lacunte Holmes",
+						"Zach Harrison McRae",
+					],
+					isMain: true
+				},
+				{
+					name: "Tattnall Square Academy",
+					students: [
+						"Calvin Brindley",
+						"Emily Mixon",
+						"Beijun DeSai",
+						"Dalton Ford",
+						"Alan Sewell",
+						"William Stubbs",
+						"Caroline Dunn",
+						"Perry Hamlin",
+					],
+					isMain: true
+				},
+				{
+					name: "Marion Co. Middle/High School",
+					students: [
+						"Lavonia Dixon",
+						"Haley Hynes",
+						"Matthew Sims",
+						"Raven Jones",
+						"Jessica Pickren",
+						"Michael Morales",
+						"Jayla Edmonds",
+						"Bense Medders",
+					],
+				},
+				{
+					name: "Schley County High School",
+					students: [
+						"Jon Arthur",
+						"Michael Leeder",
+						"Haley Sellers",
+						"Karen Weathersby",
+						"Rose Vo",
+						"Haley Welch",
+						"Whitney Welch",
+						"Joseph Logan",
+					],
+					isMain: true
+				},
+				{
+					name: "Schley County High School - ALT",
+					students: [
+						"Meghan Holloway",
+					],
+					isMain: false
+				},
+				{
+					name: "First Presbyterian Day School",
+					students: [
+						"Nicholas Shealy",
+						"Jacob Cardis",
+						"Lucas Liang",
+						"Will Martin",
+						"Trey Fountain",
+						"Madison Wiggins",
+						"David Zhang",
+						"Ruth Samuel",
+					],
+					isMain: true
+				},
+				{
+					name: "First Presbyterian Day School - ALT",
+					students: [
+						"Cooper Martin",
+						"Sam Bass",
+						"Abigail Dean",
+						"John Drinkwate",
+					],
+					isMain: false
+				},
+				{
+					name: "St. Anne Pacelli School",
+					students: [
+						"Josh Hammonds",
+						"Matt Oliver",
+						"Mary Schlies",
+						"Joseph Land",
+						"Giovanni Tanelus",
+						"Makayla Herrera",
+						"Manuel Duran",
+						"Ben  Davis",
+					],
+					isMain: true
+				},
+				{
+					name: "St. Anne Pacelli - ALT",
+					students: [
+						"Daniel Mansfield",
+						"Justus Armstrong",
+						"Matthew Dawahare",
+						"Raphael Soon",
+						"Eric Mendez",
+						"Kyle Brena",
+						"Elle Silver",
+					],
+					isMain: false
+				},
+				{
+					name: "Tattnall - ALT",
+					students: [
+						"Kenzie Dingmore",
+						"Colleen Molton",
+						"Kelly Collins",
+						"Ben Barwick",
+						"Hiroki Nishida",
+					],
+					isMain: false
+				},
+				{
+					name: "Stratford Academy",
+					students: [
+						"Shishir Bandi",
+						"Nischal Bandi",
+						"Om Sakhalkar",
+						"Pan Qi",
+						"Shijing Gong",
+						"Sachin Khurana",
+					],
+					isMain: true
+				},
+			]
+		},
+		{
+			division: "GHSA AA",
+			schools: [
+				{
+					name: "Fitzgerald High School",
+					students: [
+						"Austin Hannon",
+						"Chandler Eckler",
+						"Brannon Pate",
+						"Kassandra Dierker",
+						"Walker Smith",
+						"Jacob Duke",
+						"Jose Juarez",
+						"Mariah Rickard",
+					],
+					isMain: true
+				},
+				{
+					name: "Chattahoochee County High School",
+					students: [
+						"Joshua Edwards",
+						"Deja Crouse",
+						"Kyle Kendrick",
+						"Talibah Howard",
+						"MaryKate Jacobs",
+						"Brayden Nelson",
+						"Tyler Belle",
+						"Richard Sims",
+					],
+					isMain: true
+				},
+				{
+					name: "Thomasville High School Scholars Academy",
+					students: [
+						"Rebecca-Jane White",
+						"Jacob Reiber",
+						"Mason Hodge",
+						"Asa Harbin",
+						"Walter Tucker",
+						"Virginia Jackson",
+						"Tori Cordista",
+						"Isaac Welch",
+					],
+					isMain: true
+				},
+			]
+		},
+		{
+			division: "GHSA AAA",
+			schools: [
+				{
+					name: "Southeast Bulloch High School",
+					students: [
+						"Robin Tanaka-Blitch",
+						"Joseph Muldrew",
+						"Joseph Ham",
+						"YiNing Zheng",
+						"Wyatt Turner",
+						"Annie Mullins",
+						"Chyann Hoyle",
+						"Jared Harrision",
+					],
+					isMain: true
+				},
+				{
+					name: "Southeast Bulloch High School",
+					students: [
+						"Bri Rhodes",
+						"Sarah McCracken",
+						"Atakan Cocen",
+						"To be named later",
+					],
+					isMain: true
+				},
+				{
+					name: "Rutland High school",
+					students: [
+						"Hayley Harrod",
+						"Ryota Tsutsumi",
+						"Jessica Willing",
+						"Donnie Howard",
+						"Joseph Watson",
+						"Victoria Wooten",
+						"Emma Hanlon",
+						"Krupa Patel",
+					],
+					isMain: true
+				},
+				{
+					name: "Westside High School",
+					students: [
+						"Evan Cunningham",
+						"Callie Padget",
+						"Kevin Brown",
+						"Nicholas Norwood",
+						"Sarah Bowles",
+						"Lizzy Bernier",
+						"Lexie Irvine",
+						"Joseph Bonitabus",
+					],
+					isMain: true
+				},
+				{
+					name: "Oconee County High School",
+					students: [
+						"Student 1",
+						"Student 2",
+						"Student 3",
+						"Student 4",
+						"Student 5",
+						"Student 6",
+						"Student 7",
+						"Student 8",
+					],
+					isMain: true
+				},
+				{
+					name: "Dodge County High School",
+					students: [
+						"Jayde Mullis",
+						"Peyton Clements",
+						"Harley Johnson",
+						"Heather Yang",
+						"Jaycie Rollins",
+						"Laura Taylor",
+						"D'Na Baugh",
+						"Tanisha Washington",
+					],
+					isMain: true
+				},
+				{
+					name: "Extra School===",
+					students: [
+						"Student",
+					],
+					isMain: true
+				},
+				{
+					name: "Central-Carroll High School",
+					students: [
+						"Amelia Farmer",
+						"Liam Hulstine",
+						"Victoria Richardson",
+						"Dylan High",
+						"Casey Sprankle",
+						"Collin Grubbs",
+						"Brant Entrekin",
+						"Lauren Holm",
+					],
+					isMain: true
+				},
+				{
+					name: "Central-Carroll High School - Alt",
+					students: [
+						"Ivy Murray",
+					],
+					isMain: false
+				},
+			]
+		},
+		{
+			division: "GHSA AAAA",
+			schools: [
+				{
+					name: "Bainbridge High School",
+					students: [
+						"Grace McIntosh",
+						"David Brock",
+						"Logan Moorhead",
+						"Quest Brown",
+						"Nate Lambert",
+						"Jonathan Pyles",
+						"Jesus Juarez",
+						"Cole Prouse",
+					],
+					isMain: true
+				},
+				{
+					name: "Pike Co. High School",
+					students: [
+						"Amy Guy",
+						"Thomas Ware",
+						"Nicky Levening",
+						"Kajzah Cain",
+						"Student 5",
+						"Student 6",
+						"Student 7",
+						"Student 8",
+					],
+					isMain: true
+				},
+				{
+					name: "North Oconee High School",
+					students: [
+						"Keaton Coletti",
+						"Dawsyn Laundie",
+						"Renee Silva",
+						"Jonathan Sisson",
+						"Pam Poteh",
+						"Daniel Smith",
+						"Zida Wang",
+						"Annelissee Belvins",
+					],
+					isMain: true
+				},
+				{
+					name: "Americus Sumter SOUTH",
+					students: [
+						"Gabrion Johnson",
+						"Melvin Wiggins",
+						"Heeral McGhee",
+						"Kahria Hadley",
+						"Markusha Sparks",
+						"Prenana Kumar",
+						"Joseph Ndaayezwi",
+						"Ladarious McCray",
+					],
+					isMain: true
+				},
+				{
+					name: "Americus Sumter SOUTH - Alt",
+					students: [
+						"Na'Charlsia Floyd",
+						"Steven Karakostas",
+						"Elijah Bloomfield",
+						"Marcus Alford",
+						"Reginald Walters",
+						"Kelley Ingle",
+						"Treyvn Gray",
+						"Lamar Edwards",
+					],
+					isMain: false
+				},
+				{
+					name: "Veterans High School",
+					students: [
+						"Tyler Frye",
+						"Michael Rowlands",
+						"Kyle Kicklighter",
+						"Kiana Marlim",
+						"Nick Jenkins",
+						"Brooke Stepanek",
+						"Sarah Redfeam",
+						"Aakash Arora",
+					],
+					isMain: true
+				},
+				{
+					name: "Albany High School",
+					students: [
+						"Carterion Whitlock",
+						"Ashley Waters",
+						"Emily Beale",
+						"Jani Pollard",
+						"Gabrelle Webb",
+						"Ryan Thomas",
+						"Alaina Lewis",
+						"Alexis Stewart",
+					],
+					isMain: true
+				},
+				{
+					name: "Albany High School - Alt",
+					students: [
+						"Samuel Mackey",
+						"Chantel Mackey",
+					],
+					isMain: false
+				},
+				{
+					name: "Americus Sumter Ninth Grade Academy",
+					students: [
+						"Tyra Wiggins",
+						"Ladarisha Stone",
+						"Eric Monts",
+						"Janell Mills",
+						"Asia Cladd",
+						"Aaron Ndaayezwi",
+						"James Blanks",
+						"Taylor Garcia",
+					],
+					isMain: true
+				},
+				{
+					name: "Baldwin High School",
+					students: [
+						"David McDade",
+						"Darryl Williams",
+						"Sierra White",
+						"David Gladin",
+						"Jared Ray",
+						"Jason Zhang",
+						"Tony Johnson",
+						"Jerman Cooper",
+					],
+					isMain: true
+				},
+				{
+					name: "Baldwin High School - Alt",
+					students: [
+						"Evianna Lindsey",
+						"Bitleigh Vu",
+						"Jacob Hammock",
+						"Morgan Bilderbeck",
+						"Qadur Jones",
+						"Alexis Roberson",
+						"Meanna Harris",
+						"Austin Mitchum",
+					],
+					isMain: false
+				},
+				{
+					name: "Crisp County High School",
+					students: [
+						"Alberto Gomez",
+						"Austin Hernandez",
+						"Nathan Miles",
+						"Alexis Duckworth",
+						"Savannah Moudy",
+						"Michael Hughes",
+						"Aultman Hurt",
+						"Bailey Hernandez",
+					],
+					isMain: true
+				},
+				{
+					name: "Crisp County High School - Alt",
+					students: [
+						"Caroline Harpe",
+						"Morgan Lawson",
+						"Carley Yawn",
+					],
+					isMain: false
+				},
+				{
+					name: "Westover High School",
+					students: [
+						"Juliette Hu",
+						"Grant Brunson",
+						"Adam Clenney",
+						"Josh Moore",
+						"Yijien Ma",
+						"Jarrett Smith",
+						"Ed Belacura",
+						"Josh Hammond",
+					],
+					isMain: true
+				},
+				{
+					name: "Westover High School - Alt",
+					students: [
+						"Christine Chung",
+						"Dylan Holland",
+					],
+					isMain: false
+				},
+				{
+					name: "Thomas County Central High School",
+					students: [
+						"Christopher Wiggins",
+						"Drew Lloyd",
+						"Guanghui Li",
+						"Jacob Hancock",
+						"Jae Park",
+						"Bret Hendricks",
+					],
+					isMain: false
+				},
+				{
+					name: "North Oconee - ALT",
+					students: [
+						"Mitch Hanson",
+						"Baily Martin",
+					],
+					isMain: false
+				},
+				{
+					name: "Monroe High School",
+					students: [
+						"Zamaria Shead",
+						"Morgan Glover",
+						"Sidney Reid",
+						"Anthony Eaffora",
+						"Alex Pelico",
+						"Ajalah Davis",
+						"Jasmine Davis",
+						"Keano Jones",
+					],
+					isMain: true
+				},
+			]
+		},
+		{
+			division: "GHSA AAAAA",
+			schools: [
+				{
+					name: "Columbus High School",
+					students: [
+						"Ishan Sahoo",
+						"Ben Tucker",
+						"Elina Ebby",
+						"Trystin McCann",
+						"Joshua Barton",
+						"Jonathan VanKley",
+						"Vineeth Harith",
+						"Andrew Chambers",
+					],
+					isMain: true
+				},
+				{
+					name: "Thomas County Central High School - MOVED to 4A",
+					students: [
+						"Christopher Wiggins",
+						"Drew Lloyd",
+						"Guanghui Li",
+						"Jacob Hancock",
+						"Jae Park",
+						"Bret Hendricks",
+					],
+					isMain: true
+				},
+				{
+					name: "Houston County High School",
+					students: [
+						"George Liu",
+						"Tucker Mercier",
+						"Caleb Leborious",
+						"Morgan Carr",
+						"Jason Dykes",
+						"Nick Chalout",
+						"Matt Mercer",
+						"Chris Saetia",
+					],
+					isMain: true
+				},
+				{
+					name: "Houston County High School - ALT",
+					students: [
+						"Carter Richter",
+					],
+					isMain: false
+				},
+				{
+					name: "Columbus High - ALT",
+					students: [
+						"Aaron Wang",
+					],
+					isMain: false
+				},
+			]
+		},
+		{
+			division: "GHSA AAAAAA",
+			schools: [
+				{
+					name: "Valdosta High School",
+					students: [
+						"David Branch",
+						"Jimmy Loud",
+						"Carlos Rayan",
+						"Steven Chen",
+						"Parker Campiglia",
+					],
+					isMain: true
+				},
+				{
+					name: "Rockdale Magnet School for Science and Technology",
+					students: [
+						"Ruslan Babkin",
+						"Walt Loumakis",
+						"Andrew Bonar",
+						"Matthew Le",
+						"Molly Niemczyk",
+						"Tahmeed Hossain",
+						"Julia Long",
+						"Elijah Cheban",
+					],
+					isMain: true
+				},
+				{
+					name: "Walton High School",
+					students: [
+						"Aditya Krishnaswamy",
+						"William Zheng",
+						"Vineet Gaangireddi",
+						"Masaki Sato",
+						"Nick Lee",
+						"Daniel Marks",
+						"Evan Wen",
+						"Daniel Shu",
+					],
+					isMain: true
+				},
+				{
+					name: "Lowndes High School",
+					students: [
+						"Steven Kitchen",
+						"Jeremy Craven",
+						"Zachary Chitwood",
+						"Brooklyn Strickler",
+						"Rhett Redshaw",
+						"Mekenna Hooper",
+						"Aaron Cowger",
+						"Adam Shoemaker",
+					],
+					isMain: true
+				},
+				{
+					name: "Lowndes High School - Alt",
+					students: [
+						"Marlin Ewing",
+						"Joyce Liu",
+						"Edward Walker",
+						"Nathanael Strickler",
+					],
+					isMain: false
+				},
+			]
+		},
+		{
+			division: "GISA AA",
+			schools: [
+				{
+					name: "Georgia Christian School",
+					students: [
+						"Avery Mullins",
+						"Hans Huang",
+						"Tarah Yared",
+						"Moyuan Wang",
+						"Amber Small",
+						"Kirk Bacener",
+						"Yong Bin Kim",
+						"Toe Castellucci",
+					],
+					isMain: true
+				},
+				{
+					name: "Brandon Hall School",
+					students: [
+						"Tianqi Zhao",
+						"Jason Zhang",
+						"Mason Fugger",
+						"Tracy Mao",
+						"Lan Lin",
+						"Shunyang Liu",
+						"Jingxuan Yang",
+						"Ruiyang Zhao",
+					],
+					isMain: true
+				},
+				{
+					name: "Flint River Academy",
+					students: [
+						"Jonathn Phillips",
+						"Max Jarck",
+						"Joe Friar",
+						"Lisa Davis",
+						"chase Jarck",
+						"Colton Adamson",
+						"Hunter Tucker",
+						"Handley Davis",
+					],
+					isMain: true
+				},
+				{
+					name: "Flint River Academy - Alt",
+					students: [
+						"Baylor Elliott",
+						"Nathan Pollard",
+						"Shania Lemke",
+						"Dylan Manning",
+						"Abby Anthony",
+						"Ivie Hausman",
+						"Bryce Brett",
+						"Henry Daniel",
+					],
+					isMain: false
+				},
+				{
+					name: "Flint River Academy - Alt 2",
+					students: [
+						"Mary Grace Johnson",
+						"Cullen Evans",
+					],
+					isMain: false
+				},
+				{
+					name: "Cristo Rey Atlanta Jesuit High School",
+					students: [
+						"Mariano, Lizette",
+						"Duarte-Perdomo, Abraham",
+						"Fracer,Jahari",
+						"Loredo, Jovany",
+						"Ruiz, Dariana",
+						"Bresyda Neri",
+						"TzinTzun Zirangua, Nancy",
+						"Hai, Shalme",
+					],
+					isMain: true
+				},
+				{
+					name: "Cristo Rey Atlanta Jesuit High School - Alt",
+					students: [
+						"Chalrey Lugo",
+						"Samrawit Tesfaye",
+						"Ramos, Viviana",
+						"Espinoza Hernandez, Xiadani",
+					],
+					isMain: false
+				},
+			]
+		},
+		{
+			division: "GISA AAA",
+			schools: [
+				{
+					name: "Deerfield Windsor School",
+					students: [
+						"Lanier Freeman",
+						"Helena Augenstein",
+						"Taylor Richter",
+						"Maggie Ricks",
+						"Hannah Takash",
+						"Emma Goldsmith",
+						"Kendyll Freeman",
+						"Mary Rachel Bonner",
+					],
+					isMain: true
+				},
+				{
+					name: "Southland Academy",
+					students: [
+						"Sahir Shahryar",
+						"Matthew Tan",
+						"Mitchel Goodin",
+						"Zayd Hasnain",
+						"Alex Hooks",
+						"Jayna Patel",
+						"Kurtis Andrews",
+						"Darshti Patel",
+					],
+					isMain: true
+				},
+				{
+					name: "Southland Academy - Alt",
+					students: [
+						"Emily Brown",
+						"Tami Smith",
+						"Lousie Quillope",
+						"Brandon Revell",
+						"Anjum Alam",
+						"Naina Ghai",
+						"Morgan Youngdale",
+						"George Wall",
+					],
+					isMain: false
+				},
+				{
+					name: "Valwood School",
+					students: [
+						"Maggie Smith",
+						"Olivia Cox",
+						"Sarah Goswin",
+						"Katherine Godbee",
+						"Addy Pearlman",
+						"Paul Johnson",
+						"Chase Lin",
+						"Ana Ma",
+					],
+					isMain: true
+				},
+				{
+					name: "Valwood School - Alt",
+					students: [
+						"Hank Yu",
+						"Kush Patel",
+					],
+					isMain: false
+				},
+				{
+					name: "Southland Academy - Alt 2",
+					students: [
+						"Deepa Patel",
+						"Priyal Patel",
+						"Anslee Barnes",
+					],
+					isMain: false
+				},
+			]
+		},
+	],
+	rawData: [
+	]
+};
